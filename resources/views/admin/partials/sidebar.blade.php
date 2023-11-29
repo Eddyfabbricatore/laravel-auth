00e5c2cc96fe228ebd5a55f1a1b0dcac0f6c8b84
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-11-29 14:50:32 +0100

    [add] migrations

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="bg-dark">
-    <nav>
-        <ul>
+    <nav class="d-flex justify-content-center">
+        <ul class="m-0 p-0">
             <li><a href="{{ route('admin.home') }}">Dashboard</a></li>
             <li><a href="#">Lista post</a></li>
         </ul>
